792ecfa05a7fa5b5206c3c655f3ce88c2833c1c0
10tera <tyyt151123@gmail.com>
2023-07-12 22:44:32 +0900

    chore:ScrollButtonクリック時の挙動

--- src/components/ScrollButton/ScrollButton.tsx
+++ src/components/ScrollButton/ScrollButton.tsx
@@ -2,21 +2,28 @@ import {css} from "@emotion/react";
 
 type ScrollButtonProps = {
     buttonText:string,
+    onClick?: () => void,
 };
 
 const ButtonStyle = css({
-    border: "1px solid #EDEEF0",
+    border: "1px solid #383E47",
     borderRadius: "12px",
-    backgroundColor: "#383E47",
+    backgroundColor: "#242529",
     padding: "11px 15px",
     color: "#EDEEF0",
     fontSize: "24px",
-    fontFamily: "inherit"
+    fontFamily: "inherit",
+    ":hover":{
+        backgroundColor: "#383E47",
+    }
 });
 
-const ScrollButton = ({buttonText}: ScrollButtonProps) => {
+const ScrollButton = ({buttonText,onClick}: ScrollButtonProps) => {
+    const handleScrollButtonClick = () => {
+        if(onClick)onClick();
+    }
     return(
-        <button css={ButtonStyle}>{buttonText}</button>
+        <button css={ButtonStyle} onClick={handleScrollButtonClick}>{buttonText}</button>
     );
 };
 
